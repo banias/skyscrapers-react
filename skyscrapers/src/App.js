18dde13b00bad0957dea460e987c4f72e903feb5
Dawid Komorowski <dawidkomorowski@outlook.com>
2017-11-23 20:31:52 +0100

    Rubbish refactor and formatting.

--- skyscrapers/src/App.js
+++ skyscrapers/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      hintCells: Array(16).fill(1),
+      hintCells: Array(16).fill(0),
       skyscrapersCells: Array(16).fill(0),
     };
   }
@@ -21,11 +21,11 @@ class App extends Component {
     });
   }
 
-renderSkyscraperCell(index) {
-  return (
-    <SkyscraperCell value={this.state.skyscrapersCells[index]}/>
-  )
-}
+  renderSkyscraperCell(index) {
+    return (
+      <SkyscraperCell value={this.state.skyscrapersCells[index]}/>
+    )
+  }
 
   handleClick(i) {
     const cells = this.state.hintCells.slice();
@@ -36,61 +36,61 @@ renderSkyscraperCell(index) {
     });
   }
 
-renderSkyscraperHint(i) {
-  return (
-    <SkyscraperHint value={this.state.hintCells[i]} onClick={() => this.handleClick(i)} />
-  )
-}
+  renderSkyscraperHint(i) {
+    return (
+      <SkyscraperHint value={this.state.hintCells[i]} onClick={() => this.handleClick(i)} />
+    )
+  }
+
   render() {
     return (
       <div className="App">
- <div className="flex-skyscrapperHintRow">
-   {this.renderSkyscraperHint(0)}
-   {this.renderSkyscraperHint(1)}
-    {this.renderSkyscraperHint(2)}
-   {this.renderSkyscraperHint(3)}
-   </div>
- <div className="flex-container">
-   {this.renderSkyscraperHint(15)}
-      {this.renderSkyscraperCell(0)}
-      {this.renderSkyscraperCell(1)}
-      {this.renderSkyscraperCell(2)}
-      {this.renderSkyscraperCell(3)}
-   {this.renderSkyscraperHint(4)}
- </div>
-
-  <div className="flex-container">
-   {this.renderSkyscraperHint(14)}
-      {this.renderSkyscraperCell(4)}
-      {this.renderSkyscraperCell(5)}
-      {this.renderSkyscraperCell(6)}
-      {this.renderSkyscraperCell(7)}
-   {this.renderSkyscraperHint(5)}
-</div>
- <div className="flex-container">
-   {this.renderSkyscraperHint(13)}
-      {this.renderSkyscraperCell(8)}
-      {this.renderSkyscraperCell(9)}
-      {this.renderSkyscraperCell(10)}
-      {this.renderSkyscraperCell(11)}
-   {this.renderSkyscraperHint(6)}
-</div>
-    <div className="flex-container">
-   {this.renderSkyscraperHint(12)}
-      {this.renderSkyscraperCell(12)}
-      {this.renderSkyscraperCell(13)}
-      {this.renderSkyscraperCell(14)}
-      {this.renderSkyscraperCell(15)}
-   {this.renderSkyscraperHint(7)}
-  </div>
-   <div className="flex-skyscrapperHintRow">
-      {this.renderSkyscraperHint(8)}
-      {this.renderSkyscraperHint(9)}
-      {this.renderSkyscraperHint(10)}
-      {this.renderSkyscraperHint(11)}
-   </div>
-<GogoPowerRangers onClick={()=>this.rollRandomState()}/>
-</div>
+        <div className="flex-skyscrapperHintRow">
+          {this.renderSkyscraperHint(0)}
+          {this.renderSkyscraperHint(1)}
+          {this.renderSkyscraperHint(2)}
+          {this.renderSkyscraperHint(3)}
+        </div>
+        <div className="flex-container">
+          {this.renderSkyscraperHint(15)}
+            {this.renderSkyscraperCell(0)}
+            {this.renderSkyscraperCell(1)}
+            {this.renderSkyscraperCell(2)}
+            {this.renderSkyscraperCell(3)}
+          {this.renderSkyscraperHint(4)}
+        </div>
+        <div className="flex-container">
+          {this.renderSkyscraperHint(14)}
+            {this.renderSkyscraperCell(4)}
+            {this.renderSkyscraperCell(5)}
+            {this.renderSkyscraperCell(6)}
+            {this.renderSkyscraperCell(7)}
+          {this.renderSkyscraperHint(5)}
+        </div>
+        <div className="flex-container">
+          {this.renderSkyscraperHint(13)}
+            {this.renderSkyscraperCell(8)}
+            {this.renderSkyscraperCell(9)}
+            {this.renderSkyscraperCell(10)}
+            {this.renderSkyscraperCell(11)}
+          {this.renderSkyscraperHint(6)}
+        </div>
+        <div className="flex-container">
+          {this.renderSkyscraperHint(12)}
+            {this.renderSkyscraperCell(12)}
+            {this.renderSkyscraperCell(13)}
+            {this.renderSkyscraperCell(14)}
+            {this.renderSkyscraperCell(15)}
+          {this.renderSkyscraperHint(7)}
+        </div>
+        <div className="flex-skyscrapperHintRow">
+          {this.renderSkyscraperHint(8)}
+          {this.renderSkyscraperHint(9)}
+          {this.renderSkyscraperHint(10)}
+          {this.renderSkyscraperHint(11)}
+        </div>
+        <GogoPowerRangers onClick={()=>this.rollRandomState()}/>
+      </div>
     );
   }
 }
--- skyscrapers/src/SkyscraperHint.js
+++ skyscrapers/src/SkyscraperHint.js
@@ -3,9 +3,6 @@ import React, { Component } from 'react';
 class SkyscrapperCell extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-          value: null,
-        };
     }
 
     render() {
